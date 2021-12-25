e7f70bd6904ad77a5621574b051ed6ef267848f1
SangYong-Jeong <mmaf@naver.com>
2021-12-25 20:37:10 +0900

    연산자 부분까지 공부

---
+++ 숫자처리함수.py
@@ -0,0 +1,11 @@
+print(abs(-5)) # 5
+print(pow(4, 2)) # 4^2 = 4*4 = 16
+print(max(5, 12, 13)) # 12
+print(min(5, 12)) # 5
+print(round(3.14)) # 3
+print(round(4.99)) # 5
+
+from math import * # python이 제공하는 math 라이브러리 안에 있는 모든것(*)을 이용한다는 의미
+print(floor(4.99)) # 내림. 4
+print(ceil(3.14)) # 올리 4
+print(sqrt(16)) # 제곰근, 4
---
+++ 퀴즈(연산자).py
@@ -0,0 +1,7 @@
+from random import * 
+
+offline = randint(4, 28)
+# offline = randrange(4, 29)
+
+print("오프라인 스터디 모임 날짜는 매월 " + str(offline) + " 일로 선정되었습니다.")
+print("오프라인 스터디 모임 날짜는 매월", str(offline) ,"일로 선정되었습니다.")
